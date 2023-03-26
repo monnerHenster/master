814d15da85b876990fbf682941f9e6eaa240ce70
monkeyxox88@gmail.com <monkeyxox88@gmailcom>
2023-03-26 11:57:24 +0800

    prosure save

--- __init__.py
+++ __init__.py
@@ -11,6 +11,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+import bpy
+import random
+
 bl_info = {
     "name" : "PanelPratise",
     "author" : "M",
@@ -21,9 +24,22 @@ bl_info = {
     "warning" : "",
     "category" : "Generic"
 }
+# bpy.props.
+# 通过remap获得bonechains信息
+# 将信息储存到scene的collisionproperty中
+# 用两个collection来保存bonechain，因为是一个数列里面还有一个数列
+# 用check和label来表示source中的chain
+# 然后去除对应的不要的chain
+# 最后用label source enumerate target来匹配chain的手动选择
+# 用新的collection来做匹配关系
+# 按照chain的对应方式来添加copy rotation
+# 
+# 待加入流程：
+# auto scale
+# chain的bone的数量怎么一一对应中
+# 做智能化对应 
 
-import bpy
-import random
+
 
 def getItem(self,context):
     items = []
@@ -43,54 +59,56 @@ class enumAdd(bpy.types.PropertyGroup):
     string : bpy.props.StringProperty()
 
 #define what the property in the colection will look like
-class scad_params(bpy.types.PropertyGroup):
-    materialname: bpy.props.StringProperty(name="Material", default="")
-    slot: bpy.props.IntProperty(name="Slot")
+
+# bpy.context.scene.globalvalue = 3
+
+
+class boneChain(bpy.types.PropertyGroup):
+    string:bpy.props.StringProperty()
+
+class boneChains(bpy.types.PropertyGroup):
+    boneChain:bpy.props.CollectionProperty(type=boneChain)
+
+class boneChainsMap(bpy.types.PropertyGroup):
     checked: bpy.props.BoolProperty(name="")
-    boneName: bpy.props.StringProperty(name='')
-
-    # def getItem(self,context,newItems):
-    #     items = [
-    #         ('NONE', 'None', "Flat geometry"),
-    #         ('GEOM', 'Geometry', "Use z value from shape geometry if exists"),
-    #         ('FIELD', 'Field', "Extract z elevation value from an attribute field"),
-    #         ('OBJ', 'Object', "Get z elevation value from an existing ground mesh")
-    #     ]
-    #     return items
-
+    # boneChains: bpy.context.scene.my_boneChains
+    boneChains: bpy.props.CollectionProperty(type=boneChains)
     targetBoneChainsEnum: bpy.props.EnumProperty(items = getItem)
 
 #operator, connected the button that adds the params
 class HiOp(bpy.types.Operator):
     bl_idname = 'hi.go'
-    bl_label = 'add a thing'
+    bl_label = 'show bonechains'
     slot: bpy.props.IntProperty()
     
     def execute(self, context):
+        bpy.context.scene.my_boneChainsMap.clear()
+        item = bpy.context.scene.my_boneChainsMap.add()
+        # item.boneChains = bpy.context.scene.my_boneChains.add()
+        # item.boneChains.boneChain = bpy.context.scene.my_boneChain.add()
+        # item.boneChains.boneChain.string = 'boneName'
+
+
         #add the param to the registered collection
-        # context.scene.scad_params.add().slot = self.slot
-        context.scene.scad_params.clear()
-        context.scene.enum_Add.clear()
-       
-        # for idx
-        i = 0
-        while i < 6:
-            item = context.scene.enum_Add.add()
-            item.string = "dddd"+str(i)
-            print(context.scene.enum_Add)
-            # item.boneName = str(random.randint(1,100))
-            # item.targetBoneChainsEnum =  enumerate([ddd])
-        #     [
-        #     ('NONE', 'None', "Flat geometry"),
-        #     ('GEOM', 'Geometry', "Use z value from shape geometry if exists"),
-        #     ('FIELD', 'Field', "Extract z elevation value from an attribute field"),
-        #     ('OBJ', 'Object', "Get z elevation value from an existing ground mesh")
-        # ]
-            i += 1
-        item = context.scene.scad_params.add()
-        
-        # context.scene.scad_params.add().checked
-        # context.scene.scad_params.remove(0)
+        # context.scene.boneChainsMap.add().slot = self.slot
+        # i = 0
+        # while i < 6:
+        #     item = context.scene.enum_Add.add()
+        #     item.string = "dddd"+str(i)
+        #     # print(context.scene.enum_Add)
+        #     # item.boneName = str(random.randint(1,100))
+        #     # item.targetBoneChainsEnum =  enumerate([ddd])
+        # #     [
+        # #     ('NONE', 'None', "Flat geometry"),
+        # #     ('GEOM', 'Geometry', "Use z value from shape geometry if exists"),
+        # #     ('FIELD', 'Field', "Extract z elevation value from an attribute field"),
+        # #     ('OBJ', 'Object', "Get z elevation value from an existing ground mesh")
+        # # ]
+        #     i += 1
+        # item = context.scene.boneChainsMap.add()
+        # item.boneName = "aaaa"
+        # context.scene.boneChainsMap.add().checked
+        # context.scene.boneChainsMap.remove(0)
 
         # newItems
         return {'FINISHED'}
@@ -101,23 +119,44 @@ class TestOP(bpy.types.Operator):
     
     def execute(self, context):
         #add the param to the registered collection
-        # context.scene.scad_params.add().slot = self.slot
-        idxNeedRmove = []
+        # context.scene.boneChainsMap.add().slot = self.slot
+        # idxNeedRmove = []
 
-        for idx,item in enumerate(bpy.context.scene.scad_params):
-            if item.checked:
+        # for idx,item in enumerate(bpy.context.scene.boneChainsMap):
+        #     if item.checked:
 
-                # print(item.boneName)
-                # print(idx)
-                idxNeedRmove.append(idx)
-        i = 0
-        for idx in idxNeedRmove:
-            idx -= i
-            bpy.context.scene.scad_params.remove(idx)
-            i += 1
-        print(idxNeedRmove)
-        # context.scene.scad_params.add().checked
-        # context.scene.scad_params.remove(0)
+        #         # print(item.boneName)
+        #         # print(idx)
+        #         idxNeedRmove.append(idx)
+        # i = 0
+        # for idx in idxNeedRmove:
+        #     idx -= i
+        #     bpy.context.scene.boneChainsMap.remove(idx)
+        #     i += 1
+        # print(idxNeedRmove)
+
+        # for item in enumerate(context.scene.boneChainsMap):
+        #     attr = [a for a in dir(item) if  not a.startswith('__')]
+        #     for str in attr:
+        #         print(str,getattr(item,str))
+        #         attr2 = [b for b in dir(str) if  not b.startswith('__')]
+        #         for str2 in attr2:
+        #             print(str2,getattr(str,str2))
+
+
+        #     break
+        # for idx,item in enumerate(context.scene.boneChainsMap):
+        #     print(item.boneName)
+        #     print(item.targetBoneChainsEnum)
+        #     attr = [a for a in dir(item) if  not a.startswith('__')]
+        #     for str in attr:
+        #         print(str,getattr(item,str))
+
+        for idx,item in enumerate(bpy.context.scene.globalvalue):
+            pass
+            print(item.string)
+        # context.scene.boneChainsMap.add().checked
+        # context.scene.boneChainsMap.remove(0)
         return {'FINISHED'}        
 
 class HelloWorldPanel(bpy.types.Panel):
@@ -137,13 +176,14 @@ class HelloWorldPanel(bpy.types.Panel):
         row.label(text="Hello world!", icon='WORLD_DATA')
         # item = context.scene.enum_ 
         #use the registered collection shared with the operator to built the rest of the panel
-        for index, input in enumerate(context.scene.scad_params):
+        for index, input in enumerate(context.scene.my_boneChainsMap):
             row = layout.row()
             # row.prop(input,input.boneName)
-            row.prop(input,"checked")
+            row.prop(input,"boneChains")
+            # row.prop(input,"checked")
             # row.label(text="boneName")
-            row.label(text=input.boneName)
-            row.prop(input,"targetBoneChainsEnum",text='')
+            # row.label(text=input.boneName)
+            # row.prop(input,"targetBoneChainsEnum",text='')
             # layout.prop(input,text=input.boneName)
             # layout.prop(text=input.boneName)
             # row.prop_search(input, "materialname",bpy.data, "materials")
@@ -157,16 +197,25 @@ class HelloWorldPanel(bpy.types.Panel):
         row.operator("hi.go")
         row.operator("test.go")
 
+classes = [boneChain,boneChains,boneChainsMap]
 
 def register():
     bpy.utils.register_class(HelloWorldPanel)
     bpy.utils.register_class(TestOP)
     bpy.utils.register_class(HiOp)
-    bpy.utils.register_class(scad_params)
+    # bpy.utils.register_class(boneChainsMap)
     bpy.utils.register_class(enumAdd)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    # bpy.utils.register_class(boneChain)
+    # bpy.utils.register_class(boneChains)
     #register a collection of params so that the operator and panel can share them
-    bpy.types.Scene.scad_params = bpy.props.CollectionProperty(type=scad_params)
+    # bpy.types.Scene.boneChainsMap = bpy.props.CollectionProperty(type=boneChainsMap)
     bpy.types.Scene.enum_Add = bpy.props.CollectionProperty(type=enumAdd)
+    bpy.types.Scene.my_boneChains = bpy.props.CollectionProperty(type=boneChains)
+    # bpy.types.Scene.my_boneChains = bpy.props.CollectionProperty(type=boneChains)
+    bpy.types.Scene.my_boneChainsMap = bpy.props.CollectionProperty(type=boneChainsMap)
+
 
 
 def unregister():
@@ -174,9 +223,15 @@ def unregister():
     bpy.utils.unregister_class(HiOp)
     bpy.utils.unregister_class(TestOP)
     bpy.utils.unregister_class(enumAdd)
-    bpy.utils.unregister_class(scad_params)
-    del bpy.types.Scene.scad_params
+    # bpy.utils.unregister_class(boneChainsMap)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    # bpy.utils.unregister_class(myString)
+    # bpy.utils.unregister_class(myString)
+    # del bpy.types.Scene.boneChainsMap
     del bpy.types.Scene.enum_Add
+    del bpy.types.Scene.my_boneChains
+    del bpy.types.Scene.my_boneChainsMap
 
 if __name__ == "__main__":
     register()
